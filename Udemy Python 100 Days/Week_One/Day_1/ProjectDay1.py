c6d57911630c99792eb120d3c683faf1266157d3
Lamsauce <lilylam743@gmail.com>
2021-10-17 13:44:34 -0700

    First commit

---
+++ Udemy Python 100 Days/Week_One/Day_1/ProjectDay1.py
@@ -0,0 +1,15 @@
+# BAND NAME GENERATOR
+
+#1. Create a greeting for your program.
+print("Welcome to the band generator!")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the city you grew up in?\nCity Name: ")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet?\nPet Name: ")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is: " + city + " " + pet)
+
+#5. Make sure the input cursor shows on a new line.
