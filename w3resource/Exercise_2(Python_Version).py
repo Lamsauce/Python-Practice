aa9403399dd2a703f5bfadfdbb0b15b0ce635366
Lamsauce <lilylam743@gmail.com>
2021-10-17 13:56:08 -0700

    w3resource exercise 2 complete

---
+++ w3resource/Exercise_2(Python_Version).py
@@ -0,0 +1,11 @@
+"""
+===== EXERCISE #2 =====
+Write a Python program to get the Python version you are using.
+
+"""
+
+import sys
+print("Python Version")
+print(sys.version)
+print("Version Information")
+print(sys.version_info)
