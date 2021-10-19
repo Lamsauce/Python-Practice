431c8a4ae9a335637b4bd0959f7e4e3b3ed74319
Lamsauce <lilylam743@gmail.com>
2021-10-18 18:07:20 -0700

    Day 2 Udemy Complete

---
+++ Udemy Python 100 Days/Week_One/Day_2/Exercise_2(Life_in_weeks).py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+year = 90
+years = year - int(age)
+
+months = 12 * years
+days = 365 * years
+
+print(f"You have {years} years / {months} months / {days} days left. Live them well!")
